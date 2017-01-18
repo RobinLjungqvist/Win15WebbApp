825f92b0dd1cae95aa19299e6a2e743afcb9c067
Therese Sjögren <nike_8@hotmail.com>
2017-01-18 14:24:12 +0100

    added mockupItemRepository

---
+++ WebbAppGrp3/WebbApp/Mockup/Repo/MockupItemRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebbApp.Mockup.Interfaces;
+using WebbApp.Mockup.Models;
+
+namespace WebbApp.Mockup.Repo
+{
+    public class MockupItemRepository : IItemRepository
+    {
+        public void CreateOrUpdateItem(MockupItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MockupItem> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MockupItem GetItemByID(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MockupItem GetItemByRegion(string Region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveItemByID(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
