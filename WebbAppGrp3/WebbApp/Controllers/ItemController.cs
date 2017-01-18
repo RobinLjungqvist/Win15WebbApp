1e95b7e9e0b8f9688873ad6bdeda2ec8d1fa8fe2
Drunkenwhisp <Kevinkarlberg@hotmail.se>
2017-01-18 13:39:33 +0100

    Working on partial views regarding image upload

---
+++ WebbAppGrp3/WebbApp/Controllers/ItemController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebbApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+        // GET: Item
+        public ActionResult NewImage(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                // Additional information should be added to the filename here to specify the userID, UserIdentity
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/Images/profile"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+
+            }
+            // after successfully uploading redirect the user
+            return RedirectToAction("actionname", "controller name");
+        }
+    
+    }
+}
