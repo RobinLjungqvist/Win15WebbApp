72eabbbd6013f8563f9f851990fb63832781ef2f
Therese Sjögren <nike_8@hotmail.com>
2017-01-18 13:32:33 +0100

    Added Category class

---
+++ WebbAppGrp3/WebbApp/Mockup/Models/MockupCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebbApp.Mockup.Models
+{
+    public class MockupCategory
+    {
+        public Guid CategoryID { get; set; }
+        public string CategortName { get; set; }
+    }
+}
