f6632d60876aeb80b23820182c51e1704f269f6c
Roppin.L <robin.ljungqvist@gmail.com>
2017-01-18 12:41:46 +0100

    Interfaces and Models

---
+++ WebbAppGrp3/WebbApp/Mockup/Interfaces/MockupInterfaces.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebbApp.Mockup.Models;
+
+namespace WebbApp.Mockup.Interfaces
+{
+    public interface IUserRepository
+    {
+        MockupUser GetUserByID(Guid id);
+        void RemoveUserByID(Guid id);
+        void CreateOrUpdateUser(MockupUser user);
+
+    }
+
+    public interface IItemRepository
+    {
+        MockupItem GetItemByID(Guid id);
+
+        void RemoveItemByID(Guid id);
+
+        void CreateOrUpdateItem(MockupItem item);
+
+        MockupItem GetItemByRegion(string Region); 
+    }
+
+    public interface IMessageRepository
+    {
+        MockupMessage GetMessageByID(Guid id);
+
+        void RemoveMessageByID(Guid id);
+
+        void CreateOrUpdateMessage(MockupMessage message);
+
+    }
+
+    public interface IConversationRepository
+    {
+
+    }
+}
---
+++ WebbAppGrp3/WebbApp/Mockup/Models/MockupUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebbApp.Mockup.Models
+{
+    public class MockupUser
+    {
+
+    }
+}
