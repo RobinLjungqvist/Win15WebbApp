8547238095f332a81158e4cf9ccf57cdbac35b6f
Therese Sjögren <nike_8@hotmail.com>
2017-01-18 12:52:47 +0100

    Added ItemProperties

--- WebbAppGrp3/WebbApp/Mockup/Models/MockupItem.cs
+++ WebbAppGrp3/WebbApp/Mockup/Models/MockupItem.cs
@@ -7,5 +7,18 @@ namespace WebbApp.Mockup.Models
 {
     public class MockupItem
     {
+        public Guid ItemID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+       
+        public Guid CityID { get; set; }
+        public Guid ConditionID { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid CategoryID { get; set; }
+
+
+
     }
 }
