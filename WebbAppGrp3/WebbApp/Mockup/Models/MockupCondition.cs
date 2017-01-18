ddb6f90fdc458ca7c6cd75b609355b1d955a64f2
Therese Sjögren <nike_8@hotmail.com>
2017-01-18 13:30:48 +0100

    Added condition Class

---
+++ WebbAppGrp3/WebbApp/Mockup/Models/MockupCondition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebbApp.Mockup.Models
+{
+    public class MockupCondition
+    {
+        public Guid ConditionID { get; set; }
+        public string ConditionStatus { get; set; }
+    }
+}
