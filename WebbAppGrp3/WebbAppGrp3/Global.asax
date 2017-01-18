400943f33ae1a11d9f680a3447fe8f78cbf58d77
Roppin.L <robin.ljungqvist@gmail.com>
2017-01-18 10:37:38 +0100

    StartUp

---
+++ WebbAppGrp3/WebbAppGrp3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebbAppGrp3.MvcApplication" Language="C#" %>
---
+++ WebbAppGrp3/WebbAppGrp3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebbAppGrp3.Startup))]
+namespace WebbAppGrp3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
