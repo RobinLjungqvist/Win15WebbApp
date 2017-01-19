587f365f9b58185cc758db146563ad9a996ea1bd
mrBosse <bobjjansson@gmail.com>
2017-01-19 11:17:50 +0100

    list all items progress

--- WebbAppGrp3/WebbApp/Controllers/ItemController.cs
+++ WebbAppGrp3/WebbApp/Controllers/ItemController.cs
@@ -4,15 +4,25 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebbApp.Mockup.Interfaces;
+using WebbApp.Mockup.Repo;
+using WebbApp.ViewModels;
 
 namespace WebbApp.Controllers
 {
     public class ItemController : Controller
     {
+        private IItemRepository itemRepository;
+
+        public ItemController()
+        {
+            this.itemRepository = new MockupItemRepository();
+        }
         public ActionResult Index()
         {
             return PartialView();
         }
+
         // GET: Item
         public ActionResult NewImage(HttpPostedFileBase file)
         {
@@ -21,7 +31,7 @@ namespace WebbApp.Controllers
                 // Additional information should be added to the filename here to specify the userID, UserIdentity
                 string pic = System.IO.Path.GetFileName(file.FileName);
                 string path = System.IO.Path.Combine(
-                                       Server.MapPath("~/Images/profile"), pic);
+                    Server.MapPath("~/Images/profile"), pic);
                 // file is uploaded
                 file.SaveAs(path);
 
@@ -29,10 +39,27 @@ namespace WebbApp.Controllers
             // after successfully uploading redirect the user
             return RedirectToAction("actionname", "controller name");
         }
+
         public ActionResult DisplaySingleItem()
         {
             return View();
         }
-    
+
+        public ActionResult ListAllItems()
+        {
+            var ItemsFromRepo = itemRepository.GetAllItems();
+
+            var ViewModelItems = new List<ItemViewModel>();
+
+            foreach (var repoItem in ItemsFromRepo)
+            {
+                var newViewModel = new ItemViewModel(repoItem.ItemID, repoItem.Title, repoItem.Description, repoItem.CreateDate, repoItem.ExpirationDate, repoItem.City, repoItem.Condition, repoItem.Region, repoItem.Category, repoItem.Image);   
+
+                ViewModelItems.Add(newViewModel);
+            }
+
+
+            return PartialView(ViewModelItems);
+        }
     }
 }
---
+++ WebbAppGrp3/WebbApp/ViewModels/ItemViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebbApp.ViewModels
+{
+    public class ItemViewModel
+    {
+
+        public Guid ItemID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public string City { get; set; }
+        public string Condition { get; set; }
+        public string Region { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public ItemViewModel(Guid id, string title, string description, DateTime createdate, DateTime expirationdate,
+            string city, string condition, string region, string category, string image)
+        {
+            this.ItemID = id;
+            this.Title = title;
+            this.Description = description;
+            this.CreateDate = createdate;
+            this.ExpirationDate = expirationdate;
+            this.City = city;
+            this.Condition = condition;
+            this.Region = region;
+            this.Category = category;
+            this.Image = image;
+        }
+    }
+}
