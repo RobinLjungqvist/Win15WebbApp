02ad7e059f677e901da540487d1cba8e03b0efef
Roppin.L <robin.ljungqvist@gmail.com>
2017-01-21 14:37:43 +0100

    Added som datannotations to UserViewModel for Validation purposes.

--- WebbAppGrp3/WebbApp/ViewModels/UserViewModel.cs
+++ WebbAppGrp3/WebbApp/ViewModels/UserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,17 @@ namespace WebbApp.ViewModels
 {
     public class UserViewModel
     {
+        [Required]
+        [MinLength(5, ErrorMessage ="Must be atleast 5 characters long")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 characters")]
         public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email must be a valid Email adress.")]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public Guid id { get; set; }
     }
