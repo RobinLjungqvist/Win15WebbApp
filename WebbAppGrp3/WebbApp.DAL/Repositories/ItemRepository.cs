ac9e661a6dd02be823824ee040407696041a0b65
Roppin.L <robin.ljungqvist@gmail.com>
2017-01-23 14:11:32 +0100

    Fixed OwinStartup

--- WebbAppGrp3/WebbApp.DAL/Repositories/ItemRepository.cs
+++ WebbAppGrp3/WebbApp.DAL/Repositories/ItemRepository.cs
@@ -3,11 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebbApp.DAL.DB.Models;
 using WebbApp.DAL.Interfaces;
 
 namespace WebbApp.DAL.Repositories
 {
-    class ItemRepository
+    class ItemRepository : ISearchable<Item>
     {
+        private ApplicationContext ctx;
+
+        public ItemRepository()
+        {
+            this.ctx = new ApplicationContext();
+        }
+
+        public void Add(Item entity)
+        {
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Item entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Item> FilteredSearch(Item Searchable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Item> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Item> Search(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Item entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
