18bfd59da81d58985f6362e86973bd2d72a9e167
Roppin.L <robin.ljungqvist@gmail.com>
2017-01-23 13:52:26 +0100

    Added Item and User repository files.

---
+++ WebbAppGrp3/WebbApp.DAL/Repositories/ItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebbApp.DAL.Interfaces;
+
+namespace WebbApp.DAL.Repositories
+{
+    class ItemRepository
+    {
+    }
+}
