435a5940535cf3e2df2aa7acfa1576c145141673
Roppin.L <robin.ljungqvist@gmail.com>
2017-01-30 13:48:14 +0100

    Added a new initialmigration

---
+++ WebbAppGrp3/WebbApp.DAL/Migrations/201701301247386_NewInitialCreate.cs
@@ -0,0 +1,16 @@
+namespace WebbApp.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewInitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
