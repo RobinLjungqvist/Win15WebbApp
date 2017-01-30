8d2c7f33230a21e8e0076618fe4ae1b900a87e13
Roppin.L <robin.ljungqvist@gmail.com>
2017-01-30 20:30:13 +0100

    Added checks to prevent duplication of seed data.

--- WebbAppGrp3/WebbApp.DAL/Migrations/Configuration.cs
+++ WebbAppGrp3/WebbApp.DAL/Migrations/Configuration.cs
@@ -63,15 +63,20 @@ namespace WebbApp.DAL.Migrations
 
             var condition1 = new Condition() { ConditionId = ConditionId1, Status = "Worn" };
             var condition2 = new Condition() { ConditionId = ConditionId2, Status = "Hardly used" };
-            context.Conditions.AddOrUpdate(x => x.ConditionId, condition1, condition2);
+            if (!context.Conditions.Any())
+            {
+                context.Conditions.AddOrUpdate(x => x.ConditionId, condition1, condition2);
+            }
 
             Guid CategoryId1 = Guid.NewGuid();
             Guid CategoryId2 = Guid.NewGuid();
 
             var category1 = new Category() { CategoryId = CategoryId1, CategoryName = "Furniture" };
             var category2 = new Category() { CategoryId = CategoryId2, CategoryName = "Books" };
-            context.Categories.AddOrUpdate(x => x.CategoryId, category1, category2);
-
+            if (!context.Categories.Any())
+            {
+                context.Categories.AddOrUpdate(x => x.CategoryId, category1, category2);
+            }
             Guid RegionId1 = Guid.NewGuid();
             Guid RegionId2 = Guid.NewGuid();
 
@@ -98,42 +103,50 @@ namespace WebbApp.DAL.Migrations
                    new Region() { RegionId = Guid.NewGuid(), RegionName = "Örebro" },
                    new Region() { RegionId = Guid.NewGuid(), RegionName = "Östergötland" }
             };
-
-            foreach (Region i in regionList)
+            if (!context.Regions.Any())
             {
-                context.Regions.AddOrUpdate(x => x.RegionId, i);
+                foreach (Region i in regionList)
+                {
+                    context.Regions.AddOrUpdate(x => x.RegionId, i);
+                }
             }
 
             var ItemID1 = Guid.NewGuid();
             var ItemID2 = Guid.NewGuid();
             var ItemID3 = Guid.NewGuid();
 
+            var itemUser = context.Users.FirstOrDefault(x => x.UserName == "Admin");
+
             DateTime date = DateTime.Today;
             List<Item> itemList = new List<Item> {
                                 new Item() { ItemID = ItemID1, Title = "Bord", Description = "",
                                     CreateDate = date, ExpirationDate = date.AddDays(7),
-                                    CategoryId = CategoryId1, ConditionId = ConditionId1, RegionId = RegionId1, CityId = CityId1 } ,
+                                    CategoryId = CategoryId1, ConditionId = ConditionId1, RegionId = RegionId1, CityId = CityId1, ApplicationUser = itemUser } ,
                                 new Item() { ItemID = ItemID2, Title = "Chair", Description = "", CreateDate = date.AddDays(-1), ExpirationDate = date.AddDays(6),
-                                    CategoryId = CategoryId2, ConditionId = ConditionId2, RegionId = RegionId1, CityId = CityId1} ,
+                                    CategoryId = CategoryId2, ConditionId = ConditionId2, RegionId = RegionId1, CityId = CityId1, ApplicationUser = itemUser} ,
                                 new Item() { ItemID = ItemID3, Title = "Cupboard", Description = "", CreateDate = date.AddDays(-3), ExpirationDate = date.AddDays(4),
-                                    CategoryId = CategoryId2, ConditionId = ConditionId1, RegionId = RegionId2, CityId = CityId2}
+                                    CategoryId = CategoryId2, ConditionId = ConditionId1, RegionId = RegionId2, CityId = CityId2, ApplicationUser = itemUser}
                 };
 
-            foreach (Item i in itemList)
+            if (!context.Items.Any())
             {
-                context.Items.AddOrUpdate(x => x.ItemID, i);
+                foreach (Item i in itemList)
+                {
+                    context.Items.AddOrUpdate(x => x.ItemID, i);
+                }
             }
 
-            Guid ImageId1 = Guid.NewGuid();
+            if (!context.Images.Any())
+            {
 
-            var image1 = new Image() { ImageId = ImageId1, Path = "../Images/PlaceholderImage.png", ItemID = ItemID1 };
-            context.Images.AddOrUpdate(x => x.ImageId, image1);
-            var image2 = new Image() { ImageId = Guid.NewGuid(), Path = "../Images/PlaceholderImage.png", ItemID = ItemID2 };
-            context.Images.AddOrUpdate(x => x.ImageId, image2);
-            var image3 = new Image() { ImageId = Guid.NewGuid(), Path = "../Images/PlaceholderImage.png", ItemID = ItemID3 };
-            context.Images.AddOrUpdate(x => x.ImageId, image3);
+                var image1 = new Image() { ImageId = Guid.NewGuid(), Path = "../Images/PlaceholderImage.png", ItemID = ItemID1 };
+                context.Images.AddOrUpdate(x => x.ImageId, image1);
+                var image2 = new Image() { ImageId = Guid.NewGuid(), Path = "../Images/PlaceholderImage.png", ItemID = ItemID2 };
+                context.Images.AddOrUpdate(x => x.ImageId, image2);
+                var image3 = new Image() { ImageId = Guid.NewGuid(), Path = "../Images/PlaceholderImage.png", ItemID = ItemID3 };
+                context.Images.AddOrUpdate(x => x.ImageId, image3);
 
-
+            }
         }
     }
 }
