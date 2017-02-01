17a734e97929074dc2d60310089a5a9636c447ff
miamano <frinyo@hotmail.com>
2017-02-01 11:17:19 +0100

    Admin/expired knapp start

---
+++ WebbAppGrp3/WebbApp/Areas/Admin/Controllers/ExpiredController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebbApp.Areas.Admin.Controllers
+{
+    public class ExpiredController : Controller
+    {
+        // GET: Admin/Expired
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
