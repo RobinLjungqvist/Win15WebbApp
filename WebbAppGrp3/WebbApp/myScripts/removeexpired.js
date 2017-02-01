087453570ec221e942e082342faf0b914275bbc5
Roppin.L <robin.ljungqvist@gmail.com>
2017-02-01 13:32:08 +0100

    Added javascript for remove expired items.

---
+++ WebbAppGrp3/WebbApp/myScripts/removeexpired.js
@@ -0,0 +1,14 @@
+﻿$('#removeexpired').on('click', function (e) {
+    e.preventDefault();
+    $.ajax({
+        url: "/Admin/Expired/Index",
+        type: "POST",
+        success: function (data) {
+            alertify.alert(data);
+        },
+        error: function () {
+            alertify.error("Something went wrong");
+        }
+
+    });
+});
