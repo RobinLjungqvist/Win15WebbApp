4004a07586945c66153671e844b6ca269e244291
Roppin.L <robin.ljungqvist@gmail.com>
2017-02-01 11:53:43 +0100

    Added functionality to show email on displaysingle item

---
+++ WebbAppGrp3/WebbApp/myScripts/ShowEmail.js
@@ -0,0 +1,15 @@
+﻿$('#emailbtn').on('click',function(e){
+
+    $.ajax({
+        url: "/Item/GetUploaderEmail?UserID=" + $('#emailbtn').attr('UserID'),
+        type: "POST",
+        success: function (data) {
+            alertify.alert("Uploader Email:" + data);
+        },
+        error: function () {
+            alertify.error("Something went wrong");
+        }
+
+    });
+
+});
